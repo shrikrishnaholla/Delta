e6aa0d8b76c6d897439119d2959527ee393e34c2
Sathyam Vellal <sathyam.vellal@gmail.com>
2014-10-12 03:10:25 +0530

    Updated chat activity

--- app/src/main/java/com/delta/delta/ChatActivity.java
+++ app/src/main/java/com/delta/delta/ChatActivity.java
@@ -2,18 +2,49 @@ package com.delta.delta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.delta.delta.utils.ChatAdapter;
+import com.delta.delta.utils.ChatMessage;
+
+import java.util.ArrayList;
 
 
 public class ChatActivity extends Activity {
+
+    public ArrayList<ChatMessage> messages;
+    public ImageButton btnSendChat;
+    public EditText message;
+    public ListView chatList;
+    public ChatAdapter chatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-    }
 
+        message = (EditText) findViewById(R.id.etMessage);
+        btnSendChat = (ImageButton) findViewById(R.id.btnSendChat);
+        chatList = (ListView) findViewById(R.id.chatList);
+        messages = new ArrayList<ChatMessage>();
+        chatAdapter = new ChatAdapter(ChatActivity.this, messages);
+        chatList.setAdapter(chatAdapter);
+
+        btnSendChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chatAdapter.add(new ChatMessage(message.getText().toString()));
+                chatAdapter.notifyDataSetChanged();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/delta/delta/utils/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.delta.delta.utils;
+
+/**
+ * Created by sathyam on 10/11/14.
+ */
+public class ChatMessage {
+    public final String message;
+
+    public ChatMessage(String message) {
+        this.message = message;
+    }
+}
