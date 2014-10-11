aa319c054a6174f9466dc92c37661d72a915caf2
Sathyam Vellal <sathyam.vellal@gmail.com>
2014-10-11 19:09:04 +0530

    Starting LinkedInAuthActivity upon clicking Login!

--- app/src/main/java/com/delta/delta/LoginActivity.java
+++ app/src/main/java/com/delta/delta/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.delta.delta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,10 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(getBaseContext(), LinkedInAuthActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 
